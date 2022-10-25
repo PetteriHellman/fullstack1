ea084e8288597dc27f4c71c15ab31c4f4c269eb1
Petteri Hellman <petteri.hellman@gmail.com>
2022-10-25 23:36:19 +0300

    it worked dangit

--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -5,14 +5,18 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
-    <><p>{props.part1} {props.exercises1}{props.part2} {props.exercises2}{props.part3} {props.exercises3}</p></>
+    
+    <><p>{props.work[0]} {props.dangit[0]}</p>
+    <p>{props.work[1]} {props.dangit[1]}</p>
+    <p>{props.work[2]} {props.dangit[2]}</p></>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.dangit[0] + props.dangit[1] + props.dangit[2]}</p>
     
   )
 }
@@ -21,28 +25,32 @@ const App = () => {
 
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
 
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+    
+  ]
+    const Work = parts.map(parts => parts.name)
+    console.log(Work)
+    const Dangit = parts.map(parts => parts.exercises)
+    console.log(Dangit)
   return (
     
     <div>
       <Header course={course} />
-      <Content part1={part1.name} Content exercises1={part1.exercises}/>
-      <Content part2={part2.name} Content exercises2={part2.exercises} />
-      <Content part3={part3.name} Content exercises3={part3.exercises}/>
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+      <Content work={Work} dangit={Dangit} />
+      <Total dangit={Dangit} />
     </div>
     
   )
