139d02eb7ba7e10ffd464d55935af029031a0a29
Petteri Hellman <petteri.hellman@gmail.com>
2022-10-27 09:56:38 +0300

    1.6

--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -1,58 +1,21 @@
-const Header = (props) => {
-  return (
-      <h1> {props.course} </h1>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  console.log(props)
-  return (
-    
-    <><p>{props.work[0]} {props.dangit[0]}</p>
-    <p>{props.work[1]} {props.dangit[1]}</p>
-    <p>{props.work[2]} {props.dangit[2]}</p></>
-  )
-}
 
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.dangit[0] + props.dangit[1] + props.dangit[2]}</p>
-    
-  )
-}
 
 const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
+  const handleClick = () => {
+    setGood(1)
+  }
 
-  const course = 'Half Stack application development'
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-    
-  ]
-    const Work = parts.map(parts => parts.name)
-    console.log(Work)
-    const Dangit = parts.map(parts => parts.exercises)
-    console.log(Dangit)
   return (
-    
     <div>
-      <Header course={course} />
-      <Content work={Work} dangit={Dangit} />
-      <Total dangit={Dangit} />
+      <button onClick={handleClick}>Good</button>
     </div>
-    
   )
 }
 
