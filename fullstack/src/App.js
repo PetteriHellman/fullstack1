925a478b8ad0c084d8a41bbe791fd6f09a6b307a
Petteri Hellman <petteri.hellman@gmail.com>
2022-10-27 11:19:03 +0300

    1.8 done

--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -1,39 +1,16 @@
 import { useState } from 'react'
-
-const Good = (props) => {
+const Stats = (props) => {
   return (
+    <div>
       <div><p> Good {props.good} </p></div>
-  )
-}
-const Neutral = (props) => {
-  return (
       <div><p> Neutral {props.neutral} </p></div>
-  )
-}
-const Bad = (props) => {
-  return (
       <div><p> Bad {props.bad} </p></div>
-  )
-}
-
-const All = (props) => {
-  return (
       <div><p>All {props.bad+props.good+props.neutral}</p></div>
+      <div><p>Average {((props.good-props.bad)/(props.bad+props.good+props.neutral))}</p></div>
+      <div><p>Average positive {((props.good)/(props.bad+props.good+props.neutral))}%</p></div>
+  </div>
   )
 }
-
-const Average =(props) => {
-  return (
-    <div><p>Average {((props.good-props.bad)/(props.bad+props.good+props.neutral))}</p></div>
-  )
-}
-
-const AveragePos =(props) => {
-  return (
-    <div><p>Average positive {((props.good)/(props.bad+props.good+props.neutral))}%</p></div>
-  )
-}
-
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -56,12 +33,7 @@ const App = () => {
 
   return (
     <div>
-      <Good good={good} />
-      <Neutral neutral={neutral}/>
-      <Bad bad={bad}/>
-      <All bad={bad} good={good} neutral={neutral}/>
-      <Average bad={bad} good={good} neutral={neutral}/>
-      <AveragePos bad={bad} good={good} neutral={neutral}/>
+      <Stats bad={bad} good={good} neutral={neutral}/>
       <button onClick={handleClickGood}>Good</button>
       <button onClick={handleClickNeutral}>Neutral</button>
       <button onClick={handleClickBad}>Bad</button>
