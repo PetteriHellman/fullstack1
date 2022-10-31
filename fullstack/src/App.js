6204cfae2d91e0304de6b9d9804ace0803571a72
Petteri Hellman <petteri.hellman@gmail.com>
2022-10-31 10:33:52 +0200

    broken

--- fullstack/src/App.js
+++ fullstack/src/App.js
@@ -1,41 +1,38 @@
 
 const Course = (props) => {
+  console.log(props)
   return (
-    <div>
-      <div><p> Good {props.good} </p></div>
-      <div><p> Neutral {props.neutral} </p></div>
-      <div><p> Bad {props.bad} </p></div>
-      <div><p>All {props.bad+props.good+props.neutral}</p></div>
-      <div><p>Average {((props.good-props.bad)/(props.bad+props.good+props.neutral))}</p></div>
-      <div><p>Average positive {((props.good)/(props.bad+props.good+props.neutral))}%</p></div>
-    </div>
+    
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+    const course = {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
 
-  return <Course course={course} />
-}
+    }
+
+    const mapping = course.map(course => course.id)
+  
+    return <Course course={course} />
+  }
 
 export default App
